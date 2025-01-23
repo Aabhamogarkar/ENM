bd10d56ec339005b4b72ad0fa51e5908adb108e4
Aabhamogarkar <89242317+Aabhamogarkar@users.noreply.github.com>
2025-01-24 00:11:17 +0530

    New Username not working

--- LUGGAGE_SHARING/EShop/views.py
+++ LUGGAGE_SHARING/EShop/views.py
@@ -9,19 +9,40 @@ def HomePage(request):
 
 def SignupPage(request):
     print('before if')
-    if request.method == 'POST':
+    if request.method == 'POST':   # Signup button pressed
         uname = request.POST.get('username')
         email=request.POST.get('email')
         pass1 = request.POST.get('password1')
         pass2 = request.POST.get('password2')
-        print('after if')
 
-        print(uname,email,pass1,pass2)
+        if pass1!=pass2:
+            return HttpResponse("Passwords do not match")
+        else:
+            user = User.objects.create_user(username=uname,email=email,password=pass1)
+        print('after if')
+        my_user=User.objects.create_user(uname,email,pass1)
+        my_user.save()
+        return redirect('login')
+        # return HttpResponse('User created successfully')
+                
+
+        # print(uname,email,pass1,pass2)
        
    
     return render(request,'signup.html')
 
 def LoginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        pass1 = request.POST.get('pass')
+        user = authenticate(request,username=username,password=pass1)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            return HttpResponse("Invalid Credentials")
+        # print(username,pass1)
+
     return render(request,'login.html')
 
 # def logoutUser(request):
