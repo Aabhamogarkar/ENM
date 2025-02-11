403f65ab219b735cf9eae9c111a70fbc87ae34ac
Aabhamogarkar <89242317+Aabhamogarkar@users.noreply.github.com>
2025-02-11 23:52:52 +0530

    model.py file

--- LUGGAGE_SHARING/EShop/models.py
+++ LUGGAGE_SHARING/EShop/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+#model means database.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    # phone= models.IntegerField(None)
+    # def __init__(self, name, age, email, cell_phone=None): # Now it's okay
+    #     self.name = name
+    #     self.age = age
+    #     self.email = email
+    #     self.cell_phone = cell_phone
+    #unique means email should be unique.
+    
